194f44f86501a06c10285171d951f98c0e6b9fdb
Elisei Kamenetsky <111787903+elkamenetsky@users.noreply.github.com>
2023-02-01 22:58:44 +0300

    Update Homework_8.py

--- Homeworks/Homework_8.py
+++ Homeworks/Homework_8.py
@@ -1,17 +1,36 @@
 # Задание 1
+def function(n):
+    res = []
+    empty = []
+    for i in n:
+        if i > 0 and i % 13 == 0:
+            res.append(i)
+    if res == empty:
+        res = 'Нет подходящих значений'
+    else:
+        res = max(res)
+    return res
+
+
+print(function([2, 7, 0, 3, 1, 5, -13]))
+print(function([2, 7, 0, 3, 1, 5, -13, 13]))
+print(function([26]))
+print(function([99, 99, 100, 34, -39]))
+print(function([99, 39, 99, 100, 34]))
 
 
 # Задание 2
-# crt = ('ab', 'abcd', 'cde', 'abc', 'def')
-# s = input('-> ')
-# if s in crt:
-#     print('Yes')
-# else:
-#     print('No')
+crt = ('ab', 'abcd', 'cde', 'abc', 'def')
+s = 'ab'
+if s in crt:
+    print('Yes')
+else:
+    print('No')
 
 
 # Задание 3
-# crt = tuple(input('Введите по порядку, без пробелов, элементы кортежа: '))
-# print(crt)
-# for i in crt:
-#     print('Количество', i, '=', crt.count(i))
+num = 253523651
+crt = tuple(str(num))
+
+for i in set(crt):
+    print(f'Количество {i} = {crt.count(i)}')
